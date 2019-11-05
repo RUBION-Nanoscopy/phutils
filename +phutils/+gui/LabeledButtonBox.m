34594ba9177f5e628115e7ab147952131b84dd8f
patta42 <patrick.happel@rub.de>
2019-11-05 12:33:00 +0100

    Changed parent class

--- +phutils/+gui/LabeledButtonBox.m
+++ +phutils/+gui/LabeledButtonBox.m
@@ -1,88 +1,58 @@
-classdef LabeledButtonBox < uix.HBox
-% NAMEDBUTTONBOX A box for buttons with a label for all buttons
+classdef LabeledButtonBox < phutils.gui.LabeledBox
+% LABELEDBUTTONBOX A box for buttons with a label for all buttons
 %
     properties ( Dependent )
-        Label
-        LabelBackgroundColor
-        LabelColor
-        LabelFontSize
+        ButtonSize
     end
     
-    properties %(Access = protected)
-        bbox
-        text
-        
-    end
-    properties (SetAccess = private)
-        ready = false
-    end
+   
     methods
         function self = LabeledButtonBox( varargin )
-            bg = phutils.gui.getDefaultPanelBG()*.75;
-            fg = phutils.gui.getDefaultPanelBG()*.25;
-            
-            vb = uix.VBox( 'Parent', self );
-            self.bbox = uix.HButtonBox(...
-                'Parent', vb ...
+            self = self@phutils.gui.LabeledBox();
+            self.parentbox = uix.HButtonBox(...
+                'Parent', self.inner_box ...
             );
-            self.text = uix.Text(...
-                'Parent', vb, ...  
-                'BackgroundColor', bg, ...
-                'ForegroundColor', fg, ...
-                'String', self.Label_ ...
-            );
-            uix.Empty('Parent', self);
+            self.ready = true;
             try
                 uix.set( self, varargin{:} )
             catch e
                 delete( self )
                 e.throwAsCaller()
             end
-            self.ready = true;
-            self.Widths=[0 -1];
-            vb.Heights=[36, 16];
-        end
-       
-        function set.Label(self, l)
-            self.text.String = l;
-        end
-        function l = get.Label(self)
-            l = self.text.String;
         end
         
-        function set.LabelBackgroundColor( self, c )
-            self.text.BackgroundColor = c;
+        
+        function set.ButtonSize ( self, bs )
+            self.parentbox.ButtonSize = bs;
         end
-        function c = get.LabelBackgroundColor( self )
-            c = self.text.BackgroundColor;
+        function bs = get.ButtonSize( self )
+            bs = self.parentbox.ButtonSize;
         end
         
-        function set.LabelColor ( self, c )
-            self.text.ForegroundColor = c;
-        end
-        function c = get.LabelColor ( self )
-            c = self.text.ForegroundColor;
-        end
     end
     
     methods (Access = protected )
-        function addChild(self, child)
-            if ~self.ready
-                addChild@uix.HBox(self, child)
-            else
-                child.Parent = self.bbox;
-                n = numel(self.bbox.Children);
-                self.Widths = [ ...
-                  n * self.bbox.ButtonSize(1) ...
-                + (n-1) * self.bbox.Spacing ...
-                + 2 * self.bbox.Padding, ...
-                -1 ...
-            ];
+        function w = get_preferred_width( self )
+            try
+                n = numel( self.parentbox.Children );
+                w = n * self.ButtonSize(1) ... 
+                    + (n-1) * self.parentbox.Spacing ...
+                    + 2 * self.parentbox.Padding;
+            catch
+                w = self.PreferredWidth_;
             end
         end
-        function redraw( self )
-            
-            redraw@uix.HBox(self);
+        function h = get_preferred_height(self)
+            try
+                oldu = self.text.Units;
+                self.text.Units = 'pixels';
+                h = self.ButtonSize(2) + ...
+                    + 2 * self.parentbox.Padding ...
+                    + self.text.Position(4);
+                self.text.Units = oldu;
+            catch
+                h = self.PreferredHeight_;
+            end
         end
     end
     
